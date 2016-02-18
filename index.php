f90b25bd824542bd6469fd53bd9e69dec4485de0
Henk Van Der Klis <Henk Van Der Klis>
2016-02-18 13:56:12 +0100

    updated

--- index.php
+++ index.php
@@ -1,12 +1,43 @@
-<!doctype HTML>
-<html>
-	<head>
-		<body>
-			<form method="post">
-            <table>
-                <fielset>
-                    <legend>Vul de gegevens in </legend>
-                    <table>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
+    <title>Compound Interest Calculator</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+    <script src="js/bootstrap.min.js"></script>
+  	<nav>
+        <div class="nav navbar-default">
+        <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Compound Interest Calculator</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+        </div>
+        </div>
+            </nav>
+	<form method="post">
+        <table>
+            <fielset>
+                <legend>Vul de gegevens in </legend>
+                  	<table>
                         <tr>
                             <td><label for="start">startbedrag</label></td>
                             <td><input type="text" size="20" maxlength="40" name="start" placeholder="Startbedrag" required></td>
@@ -20,13 +51,11 @@
                             <td><input type="text" size="20" maxlength="40" name="rente" placeholder="Rente" required></td>
                         </tr>
                     </table>
-            <input class="btn btn-default" type="submit" value="Bereken">
+            		<input class="btn btn-default" type="submit" value="Bereken">
             </fieldset>
-            </table>
-        </form>
-		</body>
-	</head>
+        </table>
+    </form>
+  </body>
 </html>
-
 
 
