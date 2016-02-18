abc4d292afdf23d34cd76f2700921c94ee7a2419
Henk Van Der Klis <Henk Van Der Klis>
2016-02-18 13:20:57 +0100

    first update

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype HTML>
+<html>
+	<head>
+		<body>
+			<form method="post">
+            <table>
+                <fielset>
+                    <legend>Vul de gegevens in </legend>
+                    <table>
+                        <tr>
+                            <td><label for="start">startbedrag</label></td>
+                            <td><input type="text" size="20" maxlength="40" name="start" placeholder="Startbedrag" required></td>
+                        </tr>
+                        <tr>
+                            <td><label for="jaar">Aantal jaar</label></td>
+                            <td><input type="text" size="20" maxlength="40" name="jaar" placeholder="Aantal jaar" required></td>
+                        </tr>
+                        <tr>
+                            <td><label for="rente">Rente per jaar</label></td>
+                            <td><input type="text" size="20" maxlength="40" name="rente" placeholder="Rente" required></td>
+                        </tr>
+                    </table>
+            <input class="btn btn-default" type="submit" value="Bereken">
+            </fieldset>
+            </table>
+        </form>
+		</body>
+	</head>
+</html>
+
+
+
