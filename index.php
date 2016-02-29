71d373374e2bc46175b2b281500b54651fd1692d
Henk Van Der Klis <Henk Van Der Klis>
2016-02-29 10:17:34 +0100

    updated again

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     </head>
     <body>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="js/bootstrap.js"></script>s
+        <script src="js/bootstrap.js"></script>
         <header id="hgroup">
             <nav>
                 <div class="nav navbar-default">
